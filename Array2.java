abe9468e4038643226923978ee5ff7ea9795a382
shirisha-p-tech <shirishacareer123@gmail.com>
2025-05-23 17:05:13 +0530

    Update and rename Array.java to Array2.java

---
+++ Array2.java
@@ -0,0 +1,8 @@
+public class Array2{
+    public static void main(String[] args){
+        int[] arr = new int[]{10, 20, 30, 40, 50};
+        for (int i = 0; i < arr.length;i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
