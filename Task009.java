4b8d1ec4faed0f5f6281b3308a1b3cd1efca53f5
shirisha <shirishacareer123@gmail.com>
2025-06-07 00:52:19 +0530

    Greatest of 2 numbers using if-else

---
+++ Task009.java
@@ -0,0 +1,18 @@
+public class Task009 {
+    public static void main(String[] args) {
+
+
+        int num1 = 10;
+        int num2 = 20;
+        int greatest;
+
+        if (num1 > num2) {
+            greatest = num1;
+        } 
+        else {
+            greatest = num2;
+        }
+
+        System.out.println("The greatest number is: " + greatest);
+    }
+}
