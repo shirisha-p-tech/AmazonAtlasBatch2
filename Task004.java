6417e397e71b39e598024ee2b637b429a59e6b4b
shirisha <shirishacareer123@gmail.com>
2025-06-07 00:47:47 +0530

    Swap 2 numbers

---
+++ Task004.java
@@ -0,0 +1,12 @@
+public class Task004 {
+    public static void main(String[] args) {
+        int a=2;
+        int b=5;
+        int temp;
+        System.out.println("Before swap a is "+a+" and b is "+b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After  swap a is "+a+" and b is "+b);
+    }
+}
