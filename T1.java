8976b4969c24be8461e602803024bd9e83f41b94
shirisha <shirishacareer123@gmail.com>
2025-06-08 23:03:00 +0530

    first java file from intellij

--- T1.java
+++ T1.java
@@ -1,5 +1,5 @@
 public class T1 {
     public static void main(String[] args) {
-        System.out.println("Hello how are you doing");
+        System.out.println("Hllo how are you doing");
     }
 }
