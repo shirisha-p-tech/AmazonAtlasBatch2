fdb795ef0c61f9be4753e854fe079d04b13e550a
shirisha-p-tech <shirishacareer123@gmail.com>
2025-06-22 20:14:21 +0530

    Custom Exceptions

---
+++ ExceptionHandling/EH_Task008.java
@@ -0,0 +1,29 @@
+//Custom exceptions: // user defined exception:
+package ExceptionHandling;
+
+
+
+// A Class that represents user-defined exception
+class MyException extends Exception { // exception is a predefined class – parent class for MyException
+    public MyException(String m) {
+//inr age ====> scan in put ===> if clock if age <0 ==? Throw an exception
+        super(m); // calling the parent class constructor with parameters
+    }
+}
+
+// A Class that uses the above MyException
+public class EH_Task008 {
+    public static void main(String args[]) {
+        try {
+
+            // Throw an object of user-defined exception
+            throw new MyException("This is a custom exception");
+        }
+        catch (MyException ex) {
+            System.out.println("Caught");
+            System.out.println(ex.getMessage());
+        }
+    }
+}
+
+
