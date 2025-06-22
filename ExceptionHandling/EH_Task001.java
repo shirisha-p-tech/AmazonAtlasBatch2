f022121881ba0fac188fb1c21796bd1dc1dece25
shirisha <shirishacareer123@gmail.com>
2025-06-22 16:35:06 +0530

    Exception folder removed

--- ExceptionHandling/EH_Task001.java
+++
@@ -1,9 +0,0 @@
-package ExceptionHandling;
-
-public class EH_Task001 {
-        public static void main(String[] args) {
-            int[] myNumbers = new int[]{1, 2, 3};
-            System.out.println(myNumbers[10]);
-        }
-    }
-
