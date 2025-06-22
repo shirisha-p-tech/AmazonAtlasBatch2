3f287c2b3f8655288c06820e727d6f435f2b2805
shirisha <shirishacareer123@gmail.com>
2025-06-22 18:28:58 +0530

    Interface implementation

---
+++ OOPS/Task041.java
@@ -0,0 +1,35 @@
+//import java.io.*;
+// Interface Declared
+//Driver Code Ends
+interface testInterface {  
+    // public, static and final
+    final int tax = 10;
+    // public and abstract
+    void display();
+}
+
+
+// Class implementing interface
+class TestClass implements testInterface {  
+    // Implementing the capabilities of
+    // Interface
+    public void display(){ 
+      System.out.println("Myclass"); 
+    }
+}
+public class Task041
+//Driver Code Starts
+{
+    public static void main(String[] args)
+    {
+        TestClass t = new TestClass();
+        t.display();
+        System.out.println(t.tax);
+    }
+
+
+}
+
+
+
+
