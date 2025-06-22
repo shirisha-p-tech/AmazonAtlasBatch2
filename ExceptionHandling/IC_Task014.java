b1419e29e2d98ebc3f17d8f6a0f0d3228164a050
shirisha-p-tech <shirishacareer123@gmail.com>
2025-06-22 20:36:55 +0530

    inner class method returning outer class variable

---
+++ ExceptionHandling/IC_Task014.java
@@ -0,0 +1,20 @@
+
+    class OuterClass3{
+        int x = 50;
+        class InnerClass3 {
+            public int innerMethod() {
+                return x;
+            }
+        }
+    }
+
+
+    public class IC_Task014 {
+        public static void main(){
+            OuterClass3 myOuter = new OuterClass3();
+            OuterClass3.InnerClass3 myInner = myOuter.new InnerClass3();
+            System.out.println(myInner.innerMethod());
+        }
+    }
+
+
