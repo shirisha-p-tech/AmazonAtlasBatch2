0ae4f2410af1f25a7b997e2099a0e2d08fd7108e
shirisha <shirishacareer123@gmail.com>
2025-06-22 16:45:18 +0530

    Add 2 numbers

---
+++ JavaBasics/Task003.java
@@ -0,0 +1,9 @@
+public class Task003 {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        int sum = a + b;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
