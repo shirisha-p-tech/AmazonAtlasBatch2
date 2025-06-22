755f491bf50dbec2d8b668806d249ec381497b31
shirisha-p-tech <shirishacareer123@gmail.com>
2025-06-22 21:32:26 +0530

    Stream example

---
+++ IO_Task12.java
@@ -0,0 +1,14 @@
+//method reference
+import java.util.stream.*;
+public class IO_Task12 {
+
+
+        public static void main(String[] args) {
+            Stream<String> stream
+                    = Stream.of("Hello", "My", "name", "is", "Shirisha", "Perapagu");
+
+            stream.forEach(System.out::println);
+        }
+    }
+
+
