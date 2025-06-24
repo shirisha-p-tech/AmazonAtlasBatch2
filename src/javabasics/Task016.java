ffba72dc321e9103c0b78753e73d762c35952efa
shirisha <shirishacareer123@gmail.com>
2025-06-24 09:56:29 +0530

    Enums Demo1

---
+++ src/javabasics/Task016.java
@@ -0,0 +1,20 @@
+package javabasics;
+enum color{
+    red, blue, green, yellow
+}
+enum Weekdays {
+    Sunday, Monday, Tuesday
+}
+public class Task016 {
+        public static void main(String[] args) {
+            color c1 = color.yellow;
+            System.out.println(c1);
+            Weekdays c2 = Weekdays.Tuesday;
+            System.out.println(c2);
+        }
+
+
+}
+
+
+
