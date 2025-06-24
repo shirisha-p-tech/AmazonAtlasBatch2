58966a0b98763bf99782f72533331dde2c66eed4
shirisha <shirishacareer123@gmail.com>
2025-06-24 09:52:56 +0530

    Greatest of 2 numbers using ternary operator

---
+++ src/javabasics/Task006.java
@@ -0,0 +1,10 @@
+package javabasics;
+public class Task006 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        System.out.println("a is "+a+ " and b is "+b);
+        String c=a>b?"a is greater than b":"b is greater than a";
+        System.out.println(c);
+    }
+}
