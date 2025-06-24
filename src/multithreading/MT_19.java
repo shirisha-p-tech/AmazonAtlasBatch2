d76c4e46dbca976a39037903bf78ff76feaf72df
shirisha <shirishacareer123@gmail.com>
2025-06-24 18:26:17 +0530

    Thread execution

---
+++ src/multithreading/MT_19.java
@@ -0,0 +1,26 @@
+package multithreading;
+
+class MyRunnable implements Runnable{
+    @Override
+    public void run(){
+        System.out.println("Code executed in new thread via Runnable implementation");
+    }
+}
+
+class MyThread extends Thread{
+    @Override
+    public void run(){
+        System.out.println("Code executed in new thread via Thread extension");
+    }
+}
+public class MT_19 {
+    public static void main(String[] args) {
+        MyThread t =new MyThread();
+        t.start();
+
+        MyRunnable r=new MyRunnable();
+        Thread t1= new Thread(r);
+        t1.start();
+    }
+
+}
