1d972ce0224d44b46bc66ef80ee73da59f9293e3
shirisha <shirishacareer123@gmail.com>
2025-06-24 10:00:19 +0530

    Returning an array from method

---
+++ src/javabasics/Task028.java
@@ -0,0 +1,21 @@
+package javabasics;
+//Return of array from method
+public class Task028 {
+
+
+        public static void main(String args[])
+        {
+            int arr[] = m1();
+
+            for (int i = 0; i < arr.length; i++)
+                System.out.print(arr[i] + " ");
+        }
+
+        public static int[] m1()
+        {
+            // returning  array
+            return new int[] { 1, 2, 3 };
+        }
+
+
+}
