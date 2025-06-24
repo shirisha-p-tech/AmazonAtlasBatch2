dd0b597abb946915330cab4ee1d5fbe3442b9b97
shirisha <shirishacareer123@gmail.com>
2025-06-24 10:03:01 +0530

    Person class having getter and setter

---
+++ src/javabasics/Person.java
@@ -0,0 +1,18 @@
+package javabasics;
+public class Person {
+
+  
+        private String name;
+
+        // Getter
+        public String getName() {
+            return name;
+        }
+
+        // Setter
+        public void setName(String newName) {
+            this.name = newName;
+        }
+
+      
+}
