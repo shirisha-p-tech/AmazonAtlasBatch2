a4306f9f3786195cf4d7ae45517bb4bc955fed99
shirisha <shirishacareer123@gmail.com>
2025-06-24 18:57:44 +0530

    Lambda Expression

---
+++ src/iofilehandling/LE_Task009.java
@@ -0,0 +1,21 @@
+import java.lang.FunctionalInterface;
+package iofilehandling;
+public class LE_Task009 {
+    @FunctionalInterface
+    interface MyInterface{
+        //abstract method
+        double getPiValue();
+    }
+
+    public static void main(String[] args) {
+      //  declare a refernce of MyInterface
+        MyInterface ref;
+        // lambda expression
+        ref = () -> 3.1415;
+
+        System.out.println("Usage of Lambda exression Pi value: "+ ref.getPiValue());
+
+
+        }
+    }
+
