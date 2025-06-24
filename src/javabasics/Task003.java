f70235f8cf37089688d3e32d0ef88b3f8e152f85
shirisha <shirishacareer123@gmail.com>
2025-06-24 09:51:45 +0530

    Add 2 numbers

---
+++ src/javabasics/Task003.java
@@ -0,0 +1,10 @@
+package javabasics;
+public class Task003 {
+    public static void main(String[] args) {
+        int a=2;
+        int b=3;
+        int sum = a + b;
+
+        System.out.println("The sum is: " + sum);
+    }
+}
