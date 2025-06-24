d8bcb1536b4040ccc77040f03b96944b6ab6a3a1
shirisha <shirishacareer123@gmail.com>
2025-06-24 15:36:36 +0530

    ArrayIndexOutOfBoundsException demo

---
+++ src/exceptionhandling/EH_Task001.java
@@ -0,0 +1,9 @@
+package exceptionhandling;
+
+public class EH_Task001 {
+        public static void main(String[] args) {
+            int[] myNumbers = new int[]{1, 2, 3};
+            System.out.println(myNumbers[10]);
+        }
+    }
+
