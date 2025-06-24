2aeb4ce5c74d3cd7670d04b114d110ab05eac653
shirisha <shirishacareer123@gmail.com>
2025-06-24 09:58:05 +0530

    Initialise and traverse the array

---
+++ src/javabasics/Task021.java
@@ -0,0 +1,20 @@
+package javabasics;
+//Initialise and traverse the array
+public class Task021{
+         public static void main(String[] args)
+         {
+            // initializing array
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            // size of array
+            int n = arr.length;
+
+            // traversing array
+            for (int i = 0; i < n; i++)
+                System.out.print(arr[i] + " ");
+        }
+}
+
+
+
+
