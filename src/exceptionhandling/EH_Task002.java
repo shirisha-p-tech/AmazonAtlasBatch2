e172cb27e5e5868d66466ddaeef53ea250bf7dc2
shirisha <shirishacareer123@gmail.com>
2025-06-24 15:38:31 +0530

    try-catch block

---
+++ src/exceptionhandling/EH_Task002.java
@@ -0,0 +1,17 @@
+package exceptionhandling;
+
+public class EH_Task002 {
+    public static void main(String[] args) {
+        try {
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[10]);
+        }
+        catch (Exception e) {
+            System.out.println("Something went wrong."+ e.getMessage());
+        }
+
+    }
+
+}
+  
+
