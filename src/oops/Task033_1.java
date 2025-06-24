f8d8bb9261d12ff604c1ab6a7bb9130d8b6d8ab1
shirisha <shirishacareer123@gmail.com>
2025-06-24 10:35:33 +0530

    super keyword to call parent class constructor

---
+++ src/oops/Task033_1.java
@@ -0,0 +1,24 @@
+package oops;
+class Superclass{
+    int var;
+    Superclass(int var){
+        this.var = var;
+    }
+    public void getVar(){
+        System.out.println("var value in super class is "+ var);
+    } 
+}
+
+public class Task033_1 extends Superclass{
+    Task033_1(int var) {
+        super(var);
+    }
+    public static void main(String[] args){
+        Superclass sobj = new Superclass(100);
+        sobj.getVar();
+
+        Task033_1 tobj = new Task033_1(200);
+        tobj.getVar();
+    }
+}
+
