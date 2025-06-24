e3adf3c24396528b261886e9aa9dd43eeee15f35
shirisha <shirishacareer123@gmail.com>
2025-06-24 09:59:14 +0530

    Example of ArrayIndexOutOfBoundsException

---
+++ src/javabasics/Task025.java
@@ -0,0 +1,16 @@
+package javabasics;
+public class Task025 {
+        public static void main(String[] args)
+        {
+            int[] arr = new int[4];
+            arr[0] = 10;
+            arr[1] = 20;
+            arr[2] = 30;
+            arr[3] = 40;
+
+            System.out.println("Trying to access element outside the size of array");
+            //  System.out.println(arr[5]); Gives error ArrayIndexOutOfBoundsException
+        }
+
+}
+
