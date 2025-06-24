a8b3729c76d6a2143a3093323beb601b8bf018d8
shirisha <shirishacareer123@gmail.com>
2025-06-24 09:57:06 +0530

    With Getter and Setter

---
+++ src/javabasics/Task018.java
@@ -0,0 +1,8 @@
+package javabasics;
+public class Task018 {
+    public static void main(String[] args) {
+      Person myObj = new Person();
+      myObj.setName("John");            //using setters
+      System.out.println(myObj.getName());      //using getters
+    }
+  }
