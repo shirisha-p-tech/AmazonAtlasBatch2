88263d23fdc396384f02c1a118451ebb4d4a377d
shirisha <shirishacareer123@gmail.com>
2025-06-24 17:34:18 +0530

    Interrupting a thread

---
+++ src/multithreading/MT_Task03.java
@@ -0,0 +1,44 @@
+package multithreading;
+class RunnableDemo implements Runnable {
+    private Thread t;
+    private String threadName;
+    RunnableDemo( String name){  //constructor with 1 parameter
+        threadName = name;
+        System.out.println("Creating " + threadName );
+    }
+    public void run() {
+        System.out.println("Running " + threadName );
+        try {
+            for(int i = 4; i > 0; i--) {
+                System.out.println("Thread: " + threadName + ", " + i);
+// Let the thread sleep for a while.
+                Thread.sleep(5000);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Thread " + threadName + " interrupted.");
+        }
+        System.out.println("Thread " + threadName + " exiting.");
+
+    }
+    public void Thstart ()
+    {
+        System.out.println("Starting " + threadName );
+        if (t == null)
+        {
+            t = new Thread (this, threadName);
+            t.start ();
+        }
+    }
+}
+public class MT_Task03 {
+    public static void main(String args[]) {
+        RunnableDemo R1 = new RunnableDemo( "Thread-1");
+        R1.Thstart();
+        RunnableDemo R2 = new RunnableDemo( "Thread-2");
+        R2.Thstart();
+    }
+}
+
+
+
+
