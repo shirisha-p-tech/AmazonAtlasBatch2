2ce211ba52b3b43a0d5aeba3057dcffc71b4a943
shirisha <shirishacareer123@gmail.com>
2025-06-24 09:56:46 +0530

    Without Getter and Setter

---
+++ src/javabasics/Task017.java
@@ -0,0 +1,13 @@
+package javabasics;
+public class Task017 {
+    public static void main(String[] args) {
+
+        Person myObj = new Person();
+       // myObj.name = "John";        //error as Person.name is private
+       // System.out.println(myObj.name);  //error as Person.name is private
+
+    }
+}
+
+
+
