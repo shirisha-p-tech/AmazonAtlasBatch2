08b454e76af14b47f7cc25c3aa59c23c3541c58b
shirisha <shirishacareer123@gmail.com>
2025-06-24 09:42:45 +0530

    Call method 3 times

---
+++ src/javabasics/Task002.java
@@ -0,0 +1,15 @@
+package javabasics;
+public class Task002 {
+    static void add() {
+        int a=10;
+        int b=5;
+        int c=a+b;
+        System.out.println("Sum:"+c);
+    }
+    public static void main(String args[]){
+
+        add();
+        add();
+        add();
+    }
+}
