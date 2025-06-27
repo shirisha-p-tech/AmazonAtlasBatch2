830f64daf6c16a99893e8ad9164215010a4bb906
shirisha <shirishacareer123@gmail.com>
2025-06-27 12:35:48 +0530

    Reverse a String taken from user as input

---
+++ src/datastructures/Task8.java
@@ -0,0 +1,22 @@
+package datastructures;
+import java.util.Scanner;
+public class Task8 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string to reverse: ");
+        String originalString = sc.nextLine();
+
+        String reversedString = "";
+
+
+        for (int i = originalString.length() - 1; i >= 0; i--) {
+            reversedString = reversedString + originalString.charAt(i); // Append each character to the reversed string
+        }
+
+        System.out.println("Original string: " + originalString);
+        System.out.println("Reversed string: " + reversedString);
+
+        sc.close();
+    }
+}
