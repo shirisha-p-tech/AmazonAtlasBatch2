792895414a5bb738c6ff94cfc77e0b625a7e1527
shirisha <shirishacareer123@gmail.com>
2025-06-27 12:13:57 +0530

    Reverse an array

---
+++ src/datastructures/Task7.java
@@ -0,0 +1,25 @@
+package datastructures;
+
+public class Task7 {
+        public static void main(String[] args) {
+            int[] arr = {1, 2, 3, 4, 5};
+            System.out.println("Original Array");
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+            int left = 0;
+            int right = arr.length - 1;
+            while (left < right) {
+                int temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+            System.out.println();
+            System.out.println("Reversed Array");
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+        }
+}
