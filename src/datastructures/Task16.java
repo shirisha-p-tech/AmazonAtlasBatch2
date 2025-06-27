1ddb228d191678eda003295367afc12af09a6cd3
shirisha <shirishacareer123@gmail.com>
2025-06-27 19:31:16 +0530

    1.Adding null key value twice. 2.Making synchronized HashMap

---
+++ src/datastructures/Task16.java
@@ -0,0 +1,36 @@
+package datastructures;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+public class Task16{
+public static void main(String[] args){
+    HashMap<String, Integer> hp = new HashMap<>();
+            hp.put("Anitha", 101);
+            hp.put("Kavitha", 102);
+            hp.put("Meera", 103);
+            hp.put(null, 250);
+            hp.put(null, 300);
+
+            System.out.println("Using Hashmap, it will give updated null key value");
+            System.out.println("Null key value is "+hp.get(null));
+
+            System.out.println();
+            System.out.println("Hashmap contents");
+                for (Map.Entry<String, Integer> e : hp.entrySet()){
+                System.out.println(e.getKey() + " " + e.getValue());
+                }
+
+        System.out.println();
+        System.out.println("Synchronized HashMap contents");
+        Map<String,Integer> syncMap=Collections.synchronizedMap(hp);
+
+        synchronized (syncMap){
+        for (Map.Entry<String, Integer> e : syncMap.entrySet())
+                System.out.println(e.getKey() + " " + e.getValue());
+        }
+
+
+
+}
+}
