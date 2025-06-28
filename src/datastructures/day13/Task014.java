5375b79e9aad3d0c48dbe8bf138c1d502748dc68
shirisha <shirishacareer123@gmail.com>
2025-06-28 22:17:56 +0530

    //Linkedlist SplitIterator

---
+++ src/datastructures/day13/Task014.java
@@ -0,0 +1,20 @@
+//Linkedlist SplitIterator
+package datastructures.day13;
+import java.util.*;
+
+public class Task014{
+    public static void main(String[] args) {
+        LinkedList<String> lobj = new LinkedList<>();
+        lobj.add("All");
+        lobj.add("is");
+        lobj.add("well");
+
+        Spliterator<String> sitobj = lobj.spliterator();
+        //forEachRemaining is a method of Spliterator
+        System.out.println("Splitting the list:");
+        sitobj.forEachRemaining(System.out::println);
+
+    }
+}
+//Did not work as expected
+
