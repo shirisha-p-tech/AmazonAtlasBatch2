6af1d23e991b9a96dc394f7d1180cb053d94e107
shirisha <shirishacareer123@gmail.com>
2025-06-28 22:22:22 +0530

    Copy data from one map to another map

---
+++ src/datastructures/day13/Task18.java
@@ -0,0 +1,27 @@
+//Copy data from one map to another map.
+package datastructures.day13;
+
+import java.util.HashMap;
+
+public class Task18 {
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> hm1 = new HashMap<>();
+        hm1.put("ab", 1);
+        hm1.put("cd", 2);
+        hm1.put("ef", 3);
+
+        // Create a new HashMap hm2 by copying the contents of hm1
+        HashMap<String, Integer> hm2 = new HashMap<>(hm1);
+
+
+        System.out.println("HashMap1: " + hm1);
+        System.out.println("HashMap2: " + hm2);
+
+        // Modify hm3 and it does not affect hm2
+        hm2.put("zzzz",5);
+        System.out.println("After modification changing HashMap2 does not effect HashMap1");
+        System.out.println("HashMap1: " + hm1);
+        System.out.println("HashMap2: " + hm2);
+    }
+}
