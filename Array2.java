7ab374bd0683273758426033de975860f7d29158
shirisha-p-tech <shirishacareer123@gmail.com>
2025-06-30 21:41:32 +0530

    Delete Array2.java

--- Array2.java
+++
@@ -1,8 +0,0 @@
-public class Array2{
-    public static void main(String[] args){
-        int[] arr = new int[]{10, 20, 30, 40, 50};
-        for (int i = 0; i < arr.length;i++) {
-            System.out.print(arr[i]+ " ");
-        }
-    }
-}
