07ed5da4528959c08409bc3d5b77656562b414db
shirisha <shirishacareer123@gmail.com>
2025-08-15 23:18:34 +0530

    Main: Task05 - MessageUtil to store message

---
+++ src/main/java/org/example/day23/MessageUtil.java
@@ -0,0 +1,16 @@
+package org.example.day23;
+
+// Task05 - MessageUtil to store message
+
+public class MessageUtil {
+    private String message;
+
+    public MessageUtil(String message) {
+        this.message = message;
+    }
+
+    public String printMessage() {
+        return message;
+    }
+}
+
