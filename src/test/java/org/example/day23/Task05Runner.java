8c34e67175d1fd8678b73160e44ef9ed95e929d0
shirisha <shirishacareer123@gmail.com>
2025-08-15 23:31:09 +0530

    Test: TestSuiteRunner for Task05

---
+++ src/test/java/org/example/day23/Task05Runner.java
@@ -0,0 +1,21 @@
+package org.example.day23;
+
+
+// TestSuiteRunner to run the suite and output the results
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+
+@Suite
+@SelectClasses({
+        Task05Test01.class,
+        Task05Test02.class
+})
+public class Task05Runner {
+
+    // No code needed here as the suite will automatically run the selected test classes
+    // Task05Runner is only used by JUnit to collect and launch tests — it is not instantiated or executed like a regular class.
+}
+
+
