f51b585158a1f303adbaa2754064a9d1bbf735ab
shirisha <shirishacareer123@gmail.com>
2025-08-15 21:01:37 +0530

    Main: Compare two integers

---
+++ src/main/java/org/example/day23/Task04.java
@@ -0,0 +1,11 @@
+package org.example.day23;
+
+// Compare two integers
+
+public class Task04 {
+
+    public int compare(int n1, int n2) {
+        if (n1 > n2) return 1;
+        return -1;
+    }
+}
