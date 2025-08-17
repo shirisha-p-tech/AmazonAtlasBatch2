77596a7ecd26ebabf1eeb56acc3cf14ff7017d46
shirisha <shirishacareer123@gmail.com>
2025-08-17 17:57:03 +0530

    Product interface: Pizza

---
+++ src/designpatterns/creational/day23/factory/Pizza.java
@@ -0,0 +1,9 @@
+package designpatterns.creational.day23.factory;
+
+// Product interface
+public interface Pizza {
+    void preparation();
+    void baking();
+    void cutting();
+    void boxing();
+}
