df6fb45fdd0c65d9c09289404f454186dad8ae36
shirisha <shirishacareer123@gmail.com>
2025-08-17 20:08:34 +0530

    Store staff service

---
+++ src/designpatterns/structural/day25/facade/StoreStaffService.java
@@ -0,0 +1,8 @@
+package designpatterns.structural.day25.facade;
+
+// Store staff service: handles item selection
+public class StoreStaffService {
+    public void getItems(String items) {
+        System.out.println("Selecting Mangoes");
+    }
+}
