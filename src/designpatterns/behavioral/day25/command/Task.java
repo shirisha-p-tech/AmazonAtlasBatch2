43126657cca33e8f746da86d9a016c1ef02ed777
shirisha <shirishacareer123@gmail.com>
2025-08-17 20:42:54 +0530

    Receiver

---
+++ src/designpatterns/behavioral/day25/command/Task.java
@@ -0,0 +1,12 @@
+package designpatterns.behavioral.day25.command;
+
+// Receiver
+public class Task {
+    public void doo() {
+        System.out.println("do your home tasks");
+    }
+
+    public void dont() {
+        System.out.println("dont do your home tasks");
+    }
+}
