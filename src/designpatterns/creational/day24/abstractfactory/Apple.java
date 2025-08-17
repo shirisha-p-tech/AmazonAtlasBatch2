7feca8bf101d520c309acd20cf4ca742f40d18b0
shirisha <shirishacareer123@gmail.com>
2025-08-17 18:18:57 +0530

    Apple Factory

---
+++ src/designpatterns/creational/day24/abstractfactory/Apple.java
@@ -0,0 +1,13 @@
+package designpatterns.creational.day24.abstractfactory;
+
+// Apple Factory
+public class Apple {
+    public static Mobile getMobile(String model) {
+        if (model.equalsIgnoreCase("iphone16")) {
+            return new Mobile("Here is your iPhone 16");
+        } else if (model.equalsIgnoreCase("iphone16MaxPro")) {
+            return new Mobile("Here is your iPhone 16 Max Pro");
+        }
+        return new NoMobile("Apple", model);
+    }
+}
