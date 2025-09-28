7bbba8984aed4486b1d0ea33759972ee7ef4d446
Shirisha <shirishacareer123@gmail.com>
2025-09-28 19:53:04 +0530

    Task 14 - Tests the size of a collection using Hamcrest matchers

---
+++ Day36/src/test/java/org/example/DemoTest05Hamcrest.java
@@ -0,0 +1,18 @@
+// Task 14 - Tests the size of a collection using Hamcrest matchers
+package org.example;
+import org.junit.jupiter.api.Test;//collection Matcher
+
+
+import java.util.Arrays;
+import java.util.List;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+
+public class DemoTest05Hamcrest {
+    @Test
+    public void method2() {
+        List<String> custList = Arrays.asList("john", "Mary", "Sheik", "Singh");
+        assertThat(custList, hasSize(4));
+    }
+}
