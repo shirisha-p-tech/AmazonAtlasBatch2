1761e3ce8905303cf0807b8a554cb082e0e2c13f
Shirisha <shirishacareer123@gmail.com>
2025-09-28 22:29:49 +0530

    Task 02 - ToyService class

---
+++ Day37/src/main/java/org/example/ToyService.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class ToyService {
+    public String getToyName(int id) {
+        if (id == 1) {
+            return "Lego";
+        } else {
+            return getFallbackName();
+        }
+    }
+
+    public String getFallbackName() {
+        return "Unknown";
+    }
+}
