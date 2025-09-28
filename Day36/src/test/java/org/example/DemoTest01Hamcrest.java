db55adb601224e4e43dda6ec55fa25d5d698700c
Shirisha <shirishacareer123@gmail.com>
2025-09-28 19:47:17 +0530

    Task 10 - Hamcrest

---
+++ Day36/src/test/java/org/example/DemoTest01Hamcrest.java
@@ -0,0 +1,17 @@
+// Task 10 - Hamcrest
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+public class DemoTest01Hamcrest {
+    @Test
+    public void hamcrestMethod() {
+        String str1 = "Apple";
+        String str2 = "Apple";
+        assertThat(str1 ,equalToIgnoringCase(str2));
+    }
+}
