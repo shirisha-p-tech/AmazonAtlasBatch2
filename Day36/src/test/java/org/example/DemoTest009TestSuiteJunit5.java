14638427cb6d82ce8bdf21f58ca2e5b212c3232a
Shirisha <shirishacareer123@gmail.com>
2025-09-28 19:46:25 +0530

    Task 09 - Test Suite

---
+++ Day36/src/test/java/org/example/DemoTest009TestSuiteJunit5.java
@@ -0,0 +1,15 @@
+// Task 09 - Test Suite
+
+package org.example;
+
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+
+@Suite
+@SelectClasses({DemoTest008AfterBeforeJunit5.class, DemoTest007Tag_FilterJunit5.class})
+public class DemoTest009TestSuiteJunit5 {
+
+
+}
