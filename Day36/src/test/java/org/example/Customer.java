bb9c268278e614aed4a3e134e2e268b18426b6e3
Shirisha <shirishacareer123@gmail.com>
2025-09-28 19:47:56 +0530

    Customer bean

---
+++ Day36/src/test/java/org/example/Customer.java
@@ -0,0 +1,22 @@
+// Customer bean
+package org.example;
+
+
+public class Customer {
+    private String Fname;
+    private String Lname;
+    public Customer(String Fname, String Lname) {
+        this.Fname = Fname;
+        this.Lname = Lname;
+    }
+
+
+    public String getFname() {
+        return Fname;
+    }
+
+
+    public String getLname() {
+        return Lname;
+    }
+}
