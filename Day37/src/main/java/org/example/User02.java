80d842172c82c30fc13b23aa38df7b3de6cc70a8
Shirisha <shirishacareer123@gmail.com>
2025-09-28 22:01:22 +0530

    Task 01 - User02 class

---
+++ Day37/src/main/java/org/example/User02.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import java.util.Date;
+
+public class User02 {
+    private String name;
+    private Date registrationDate;
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
