3c275ad763b00c0aeb9a0a0b5ffcbccc81e8fc64
Shirisha <shirishacareer123@gmail.com>
2025-09-28 17:54:08 +0530

    Task 01 - TestCase1

---
+++ Day36/src/test/java/org/example/DemoTest001TestJunit.java
@@ -0,0 +1,23 @@
+// Task 01 - TestCase1
+package org.example;
+
+
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class DemoTest001TestJunit {
+    @Test
+    public void Testcase1() {
+        String str = "Shirisha";
+        assertEquals("Shirisha", str);
+    }
+}
+
+
+
+
+
+
