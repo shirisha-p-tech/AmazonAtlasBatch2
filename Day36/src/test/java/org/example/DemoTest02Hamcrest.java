4ed214f5be243467aedb3783c1ca58cc3cba07a4
Shirisha <shirishacareer123@gmail.com>
2025-09-28 19:49:35 +0530

    Task 11 - Tests Customer's toString method using Hamcrest matchers

---
+++ Day36/src/test/java/org/example/DemoTest02Hamcrest.java
@@ -0,0 +1,15 @@
+// Task 11 - Tests Customer's toString method using Hamcrest matchers
+
+package org.example;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;//Object Matcher:
+import static org.hamcrest.Matchers.hasToString;
+// we  are given a bean ... string correctness
+public class DemoTest02Hamcrest {
+    @Test
+    public void method1() {
+        Customer customer = new Customer("John", "Abraham");
+        String str = customer.toString();
+        assertThat(customer, hasToString(str));
+    }
+}
